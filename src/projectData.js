3aaf3d2b4e07caaec6b41f329c2ce110a6980823
Michael Winser <mike.winser@gmail.com>
2021-02-27 14:42:39 -0800

    added petals project

--- src/projectData.js
+++ src/projectData.js
@@ -1,19 +1,26 @@
+import petals from "./petals-site.png"
 const projectData = [
     {
         name:"Improv App",
-        description:"A suggestion generator for the Blue Door Theatre",
+        description:"A suggestion generator for the Blue Door Theatre using vanilla javascript",
         image:"https://d33wubrfki0l68.cloudfront.net/600b6bf3dc82370007cff818/screenshot.png",
         link:"https://bdt-improv.netlify.app/" 
     },
     {
+        name:"Ecommerce Page",
+        description:"Personal project using React Router to create a multipage responsive site with a cart and local storage",
+        image:petals,
+        link:"https://petal-site.netlify.com" 
+    },
+    {
         name:"Landing Page",
-        description:"Personal landing page project to work on responsive design",
+        description:"Expanded freeCodeCamp landing page project to work on responsive UI design",
         image:"https://assets.codepen.io/5166947/internal/screenshots/pens/XWKbapp.default.png?fit=cover&format=auto&ha=true&height=540&quality=75&v=2&version=1609729930&width=960",
         link:"https://codepen.io/winserm/full/XWKbapp" 
     },
     {
         name:"Data Visualization",
-        description:"Personal data visualization project using D3",
+        description:"freeCodeCamp data visualization project using D3",
         image:"https://assets.codepen.io/5166947/internal/screenshots/pens/XWKOxpv.default.png?fit=cover&format=auto&ha=false&height=540&quality=75&v=2&version=1605745655&width=960",
         link:"https://codepen.io/winserm/full/XWKOxpv" 
     }
