f5a46dbb6df6a45a60de4a2e8da9a7b583d73281
Michael Winser <mike.winser@gmail.com>
2021-03-14 18:17:49 -0700

    added 2 projects, adjusted size of tiles

--- src/projectData.js
+++ src/projectData.js
@@ -1,16 +1,30 @@
 import petals from "./petals-site.png"
+import castaways from "./castaways-site.png"
+import bumblr from "./bumblr.png"
 const projectData = [
+    {
+        name:"Tumblr Clone",
+        description:"A clone of Tumblr using React, Tailwind, Firebase Auth and Firestore.",
+        image:bumblr,
+        link:"https://bumblr.netlify.app/" 
+    },
+    {
+        name:"Ecommerce Site",
+        description:"Personal project using React Router to create a multipage responsive site with a cart and local storage",
+        image:petals,
+        link:"https://petal-site.netlify.com" 
+    },
+    {
+        name:"Castaways Game",
+        description:"A Survivor-style game using useReducer to manage complicated state objects",
+        image:castaways,
+        link:"https://castaways.netlify.com" 
+    },
     {
         name:"Improv App",
         description:"A suggestion generator for the Blue Door Theatre using vanilla javascript",
         image:"https://d33wubrfki0l68.cloudfront.net/600b6bf3dc82370007cff818/screenshot.png",
         link:"https://bdt-improv.netlify.app/" 
-    },
-    {
-        name:"Ecommerce Page",
-        description:"Personal project using React Router to create a multipage responsive site with a cart and local storage",
-        image:petals,
-        link:"https://petal-site.netlify.com" 
     },
     {
         name:"Landing Page",
