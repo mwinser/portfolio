ea8981ce917939ef02611513eb02be100794e318
Michael Winser <mike.winser@gmail.com>
2021-05-07 15:26:42 -0700

    small changes to design

--- src/components/projectListItem.jsx
+++ src/components/projectListItem.jsx
@@ -7,20 +7,20 @@ export default function ProjectListItem(props){
     
     return(
         <div className="project-tile" >
-        <img className="project-img" src={project.image} alt={props.name + " screenshot"} />
-        <div className="project-desc">
-            <div className="project-title">
-                <a href={project.link}><FontAwesomeIcon icon={faLink} /></a>
-                <h3>{project.name}</h3>
-                {project.git && (
-                    <a href={project.git}><FontAwesomeIcon icon={faGithub} /></a>
-                )}
-                {project.codepen && (
-                    <a href={project.codepen}><FontAwesomeIcon icon={faCodepen} /></a>
-                )}
+            <img className="project-img" src={project.image} alt={props.name + " screenshot"} />
+            <div className="project-desc">
+                <div className="project-title">
+                    <a href={project.link}><FontAwesomeIcon icon={faLink} /></a>
+                    <h3>{project.name}</h3>
+                    {project.git && (
+                        <a href={project.git}><FontAwesomeIcon icon={faGithub} /></a>
+                    )}
+                    {project.codepen && (
+                        <a href={project.codepen}><FontAwesomeIcon icon={faCodepen} /></a>
+                    )}
+                </div>
+                <p>{project.description}</p>
             </div>
-            <p>{project.description}</p>
         </div>
-    </div>
     )
 }
