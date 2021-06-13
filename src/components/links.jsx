d89bb534d036821e01ee7ea8e25a146a37ac60c8
Michael Winser <mike.winser@gmail.com>
2021-06-13 10:12:08 -0700

    aria-labels for icons

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -5,11 +5,11 @@ import { faLinkedin, faTwitter, faGithub, faCodepen} from '@fortawesome/free-bra
 export default function Links() {
     return(
         <div id="links">
-            <a href="https://github.com/mwinser" ><FontAwesomeIcon icon={faGithub} /></a>
-            <a href="https://twitter.com/winsaurus"><FontAwesomeIcon icon={faTwitter} /></a> 
-            <a href="https://linkedin.com/in/mwinser"><FontAwesomeIcon icon={faLinkedin} /></a>
-            <a href="https://codepen.io/winserm"><FontAwesomeIcon icon={faCodepen} /></a>
-            <a href="https://replit.com/@mwinser"><img width="36px" style={{"verticalAlign": "-0.2em"}} src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Repl.it_logo.svg" alt="replit logo"/></a>
+            <a href="https://github.com/mwinser" aria-label="Github"><FontAwesomeIcon icon={faGithub} /></a>
+            <a href="https://twitter.com/winsaurus" aria-label="Twitter"><FontAwesomeIcon icon={faTwitter} /></a> 
+            <a href="https://linkedin.com/in/mwinser" aria-label="LinkedIn"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href="https://codepen.io/winserm" aria-label="Codepen"><FontAwesomeIcon icon={faCodepen} /></a>
+            <a href="https://replit.com/@mwinser" aria-label="Replit"><img width="36px" style={{"verticalAlign": "-0.2em"}} src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Repl.it_logo.svg" alt="replit logo"/></a>
         </div>
     )
 }
