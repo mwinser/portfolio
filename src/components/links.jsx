0373ee0414a707d7ad981e97cfedc0c86678cfdf
Michael Winser <mike.winser@gmail.com>
2021-09-03 10:05:20 -0700

    external links open in new tab

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -5,11 +5,11 @@ import { faLinkedin, faTwitter, faGithub, faCodepen} from '@fortawesome/free-bra
 export default function Links() {
     return(
         <div id="links">
-            <a href="https://github.com/mwinser" aria-label="Github"><FontAwesomeIcon icon={faGithub} /></a>
-            <a href="https://twitter.com/winsaurus" aria-label="Twitter"><FontAwesomeIcon icon={faTwitter} /></a> 
-            <a href="https://linkedin.com/in/mwinser" aria-label="LinkedIn"><FontAwesomeIcon icon={faLinkedin} /></a>
-            <a href="https://codepen.io/winserm" aria-label="Codepen"><FontAwesomeIcon icon={faCodepen} /></a>
-            <a href="https://replit.com/@mwinser" aria-label="Replit"><img width="36px" style={{"verticalAlign": "-0.2em"}} src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Repl.it_logo.svg" alt="replit logo"/></a>
+            <a href="https://github.com/mwinser" aria-label="Github" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub} /></a>
+            <a href="https://twitter.com/winsaurus" aria-label="Twitter" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faTwitter} /></a> 
+            <a href="https://linkedin.com/in/mwinser" aria-label="LinkedIn" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href="https://codepen.io/winserm" aria-label="Codepen" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faCodepen} /></a>
+            <a href="https://replit.com/@mwinser" aria-label="Replit" target="_blank" rel="noreferrer noopener"><img width="36px" style={{"verticalAlign": "-0.2em"}} src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Repl.it_logo.svg" alt="replit logo"/></a>
         </div>
     )
 }
--- src/components/projectListItem.jsx
+++ src/components/projectListItem.jsx
@@ -8,18 +8,18 @@ export default function ProjectListItem(props){
     return(
         
             <div className="project-tile" >
-                <a href={project.link}>
+                <a href={project.link} target="_blank" rel="noreferrer noopener">
                     <div className="project-img" style={{backgroundImage: "url(" + project.image + ")"}} alt={project.name + " screenshot"} />
                 </a>
                 <div className="project-desc">
                     <div className="project-title">
-                        <a href={project.link}><FontAwesomeIcon icon={faLink} /></a>
+                        <a href={project.link} target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faLink} /></a>
                         <h3>{project.name}</h3>
                         {project.git && (
-                            <a href={project.git}><FontAwesomeIcon icon={faGithub} /></a>
+                            <a href={project.git} target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub} /></a>
                         )}
                         {project.codepen && (
-                            <a href={project.codepen}><FontAwesomeIcon icon={faCodepen} /></a>
+                            <a href={project.codepen} target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faCodepen} /></a>
                         )}
                     </div>
                     <p>{project.description}</p>
