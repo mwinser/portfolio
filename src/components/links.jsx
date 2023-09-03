b43cd7fa811c97a3f31da4f9c7ebfd5739e250de
Michael A Winser <74273216+mwinser@users.noreply.github.com>
2023-09-03 09:24:08 -0700

    bugfix

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -6,10 +6,6 @@ export default function Links() {
     return(
         <div id="links">
             <a href="https://github.com/mwinser" aria-label="Github" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faGithub} /></a>
-<<<<<<< HEAD
-            {/* <a href="https://twitter.com/winsaurus" aria-label="Twitter" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faTwitter} /></a>  */}
-=======
->>>>>>> develop
             <a href="https://linkedin.com/in/mwinser" aria-label="LinkedIn" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faLinkedin} /></a>
             <a href="https://codepen.io/winserm" aria-label="Codepen" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon icon={faCodepen} /></a>
             <a href="https://replit.com/@mwinser" aria-label="Replit" target="_blank" rel="noreferrer noopener"><img width="36px" style={{"verticalAlign": "-0.2em"}} src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Repl.it_logo.svg" alt="replit logo"/></a>
