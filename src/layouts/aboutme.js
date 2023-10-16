0af842772d74bd1185460e80309f528263d0472e
Michael A Winser <74273216+mwinser@users.noreply.github.com>
2023-10-16 12:06:15 -0700

    various copy updates

--- src/layouts/aboutme.js
+++ src/layouts/aboutme.js
@@ -12,16 +12,17 @@ function AboutMe() {
           <h1>About Me</h1>
           <p>Hi! I'm a self-taught web developer and former scientist living in the beautiful Pacific Northwest
           </p>
-          <p> During Covid, I really started reconsidering my career as I got into game and web dev. I took a year off to focus on learning and took my first web development position in 2021.
+          <p> During the pandemic, I started getting into game and web dev. I loved it so much, I decided quit my job and spent the next year learning everything I could, atking my first web development position in 2021.
           </p>
-          <p>I've always loved solving problems and making stuff, so I seriously love to code.
+          <p>I love solving problems, learning new things and making stuff, so I seriously love to code.
           </p>
           <div className="flex-cards">
+            <SkillsCard title="Passions" skills="Pixel Perfect Design Implementation, Accessibility, Prompt Engineering"/>
             <SkillsCard title="Front Skillz" skills="HTML, CSS/SASS, JS, Vue/Nuxt, React, Tailwind, GSAP, Three.js, Babylon.js, Pixi.js"/>
             <SkillsCard title="Back Skillz" skills="Node, Express, PHP, Firebase, Shopify, Wordpress, Docker"/>
-            <SkillsCard title="CMSs" skills="Prismic, Sanity, Craft, Wordpress(ACF)"/>
+            <SkillsCard title="CMSs" skills="Prismic, Sanity, Craft, Wordpress(ACF), Webflow"/>
             <SkillsCard title="Others" skills="Git, NPM, VS Code, GA/GTM, MailChimp, OpenAI API"/>
-            <SkillsCard title="Passions" skills="Pixel Perfect Design Implementation, Accessibility, Prompt Engineering"/>
+
           </div>
           </div>
       </section>
--- src/layouts/welcome.js
+++ src/layouts/welcome.js
@@ -8,7 +8,7 @@ function Welcome (){
     <section className="primary-section">
       <div id="profile-container" >
         <ProfilePic/>
-          <div className="text-tile"><h1>Hi there! I'm Mike Winser.</h1><p>I'm a web developer with a mission to bring your digital dreams to life. Whether you're a startup seeking a captivating online presence or an established brand looking to level up your web experience, I'm here to transform your ideas into pixel-perfect reality.</p>
+          <div className="text-tile"><h1>Hi there! I'm Mike Winser.</h1><p>I'm a web developer with a mission to bring your digital dreams to life!</p><p> Whether you're a startup seeking a captivating online presence or an established brand looking to level up your web experience, I'm here to transform your ideas into pixel-perfect reality.</p>
             <Links/>
           </div>
         </div>
