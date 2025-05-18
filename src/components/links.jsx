07ada303eb82ecdfd54d323c620e050d2b4abdfd
Michael A Winser <74273216+mwinser@users.noreply.github.com>
2025-05-17 17:21:37 -0700

    more linter errors

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -1,6 +1,6 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub, faCodepen} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Links() {
     return(
